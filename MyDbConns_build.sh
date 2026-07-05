ab7ebb6b7202cd1fdc0cd8b222162ae52ca3198b
Jozsef Kiss <info@kisscodesystems.com>
2026-07-05 07:48:07 +0200

    Add files via upload

--- MyDbConns_build.sh
+++ MyDbConns_build.sh
@@ -2,14 +2,14 @@
 
 # 1. Compile the sources into a fresh output directory.
 #    The Oracle JDBC driver is required on the classpath (oracle.jdbc.OracleBfile).
-DRIVER=lib/Db2Jdbc.jar:lib/MssqlJdbc.jar:lib/MysqlJdbc.jar:lib/OracleJdbc.jar:lib/PostgresqlJdbc.jar
-javac -cp "$DRIVER" -d bin src/com/kisscodesystems/MyDbConns/*.java
+DRIVERS=lib/Db2Jdbc.jar:lib/MssqlJdbc.jar:lib/MysqlJdbc.jar:lib/OracleJdbc.jar:lib/PostgresqlJdbc.jar
+javac -cp "$DRIVERS" -d bin src/com/kisscodesystems/MyDbConns/*.java
 
 # 2. Package a runnable jar using the bundled manifest (it sets Main-Class).
 cd bin && jar cvfm MyDbConns.jar ../src/com/kisscodesystems/MyDbConns/manifest.txt com/kisscodesystems/MyDbConns/*.class
 
-mv MyDbConns.jar ../
+cp MyDbConns.jar ../
 
 echo ""
 echo "You can now start your application by"
-echo "java -cp $DRIVER:MyDbConns.jar com.kisscodesystems.MyDbConns.MyDbConnsMain interactive mode"
+echo "java -cp $DRIVERS:MyDbConns.jar com.kisscodesystems.MyDbConns.MyDbConnsMain interactive mode"
